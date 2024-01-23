cb49049b8a5bcc037e921fbf0501cb77188113c3
Ronnie Gentile <rgentile09@yahoo.com>
2024-01-22 22:41:17 -0600

    Part 2 complete!

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,17 +12,9 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space? ",
-                 "True or false: 5 kilometer == 5000 meters? ",
-                  "(5 + 3)/2 * 10 = ? ", 
-                  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-                   "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride",
-                     "true", 
-                     "40", 
-                     "Trajectory",
-                     "3"]
-let candidateAnswers = "";
+let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -33,8 +25,13 @@ candidateName = input.question("Enter name: ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  
+for (let i = 0; i < questions.length; i++) {
+  candidateAnswers.push(input.question(questions[i]));
 
+ 
+  
+}
 
 
 
@@ -43,10 +40,19 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-  console.log("Answer is Correct!");
-} else {"Answer is incorrect."
-}
+  for (let i = 0; i < questions.length; i++) {
+      console.log(`Correct answer to question ${i +1}: ${correctAnswers[i]}`)
+      console.log(`Your answer: ${candidateAnswers[i]}`);
+    
+
+  }
+
+  
+
+ 
+  
+
+
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
